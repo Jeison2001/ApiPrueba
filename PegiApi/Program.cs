e141af3751dcc781e316bcac22b1297ce872dbe5
Jeison2001 <79232868+Jeison2001@users.noreply.github.com>
2023-08-17 02:22:16 -0500

    conf

--- PegiApi/Program.cs
+++ PegiApi/Program.cs
@@ -4,7 +4,7 @@ using Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,7 +36,46 @@ builder.Services.AddServices();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Pegi",
+        Version = "v1",
+        Description = "Es una api para la gestion de proyectos de investigación",
+        Contact = new OpenApiContact
+        {
+            Name = "Ana Patiño",
+            Email = "sofipatino614@gmail.com"
+        }
+    });
+
+    // Configura la seguridad para Swagger UI
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy => policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader())
@@ -48,7 +87,10 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
+    });
 }
 
 app.UseCors();
